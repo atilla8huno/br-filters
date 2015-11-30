6f0ae7d49ffb6c212a747a1c8de7740d0423b369
Atilla Barros <atilla8huno@gmail.com>
2015-11-30 12:58:59 -0200

    Added filtro de CNPJ

---
+++ src/cnpj.filter.js
@@ -0,0 +1,32 @@
+/**
+ * Created by ATILLA on 27/11/2015.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('angular-br-filters')
+        .filter('cnpj', cnpj);
+
+    /** @ngInject */
+    function cnpj() {
+
+        var cnpj = function (value) {
+            return angular.isUndefined(value) ? value : formattedCNPJ(value);
+        };
+
+        return cnpj;
+
+        function formattedCNPJ(value) {
+            var formatted = value + '';
+
+            formatted = formatted.replace(/\D/g, '');
+            formatted = formatted.replace(/^(\d{2})(\d)/, '$1.$2');
+            formatted = formatted.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+            formatted = formatted.replace(/\.(\d{3})(\d)/, '.$1/$2');
+            formatted = formatted.replace(/(\d{4})(\d)/, '$1-$2');
+
+            return formatted;
+        }
+    }
+})();
